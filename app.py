9d9af6c9838206f734994cdd3be202d6ce3b7382
Rina Tsegay <rinamts@bu.edu>
2024-07-18 17:10:59 -0400

    added functionality so that the users top 5 playists are in the featured section

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
 from backend.user_auth import create_users_table, is_valid_email, is_valid_password, register_user, login_user
 
 app = Flask(__name__)
@@ -17,12 +18,14 @@ client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 redirect_uri = 'http://localhost:8080/callback'
 scope = 'playlist-read-private,user-follow-read,user-top-read,user-read-recently-played'
 
+cache_handler = FlaskSessionCacheHandler(session)
+
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
     redirect_uri=redirect_uri,
     scope=scope,
-    cache_path='.spotifycache',
+    cache_handler=cache_handler,
     show_dialog=True
 )
 
@@ -30,9 +33,93 @@ def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     return conn
 
+@app.before_request
+def initialize_database():
+    conn = get_db_connection()
+    create_users_table(conn)
+    conn.close()
+
 @app.route('/')
 def start_page():
+    if 'username' in session:
+        return redirect(url_for('index'))
     return render_template('start-page.html')
+
+@app.route('/loginSpotify')
+def loginSpotify():
+    if 'username' not in session:
+        flash("Please log in to your account first before connecting Spotify.")
+        return redirect(url_for('login'))
+    return redirect(sp_oauth.get_authorize_url())
+
+@app.route('/callback')
+def callback():
+    if 'username' not in session:
+        flash("Please log in to your account first.")
+        return redirect(url_for('login'))
+    
+    token_info = sp_oauth.get_access_token(request.args['code'], as_dict=False)
+    session['token_info'] = token_info
+    flash('Spotify account connected successfully.', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/index')
+def index():
+    if 'username' not in session:
+        flash("Please log in to access this page.")
+        return redirect(url_for('login'))
+
+    username = session['username']
+
+    token_info = session.get('token_info', None)
+    if not token_info:
+        flash("Please connect your Spotify account.")
+        return redirect(url_for('loginSpotify'))
+
+    try:
+        sp = Spotify(auth=token_info)
+        playlists = sp.current_user_playlists(limit=5)
+        playlists_info = [(pl['name'], pl['images'][0]['url'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    except Exception as e:
+        print(f"Error fetching Spotify playlists: {e}")
+        flash("There was an error connecting to Spotify. Please try logging in again.")
+        return redirect(url_for('loginSpotify'))
+
+    return render_template('index.html', username=username, playlists_info=playlists_info)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('Logged out successfully.', 'success')
+    return redirect(url_for('start_page'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        if cursor.fetchone():
+            flash('Username already exists. Please choose a different username.', 'danger')
+        elif cursor.execute("SELECT * FROM users WHERE email = ?", (email,)).fetchone():
+            flash('Email already registered. Please use a different email.', 'danger')
+        elif not is_valid_email(email):
+            flash('Invalid email format.', 'danger')
+        elif not is_valid_password(password):
+            flash('Password must be at least 8 characters long and contain at least one special character.', 'danger')
+        else:
+            success = register_user(conn, username, email, password)
+            if success:
+                session['username'] = username
+                flash('Registration successful. Please connect a music platform.', 'success')
+                return redirect(url_for('connect'))
+            else:
+                flash('An error occurred during registration. Please try again.', 'danger')
+    return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -54,44 +141,15 @@ def login():
                 flash('Username or email does not exist. Maybe you should sign up instead.', 'danger')
     return render_template('login.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        
-        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-        if cursor.fetchone():
-            flash('Username already exists. Please choose a different username.', 'danger')
-        elif cursor.execute("SELECT * FROM users WHERE email = ?", (email,)).fetchone():
-            flash('Email already registered. Please use a different email.', 'danger')
-        elif not is_valid_email(email):
-            flash('Invalid email format.', 'danger')
-        elif not is_valid_password(password):
-            flash('Password must be at least 8 characters long and contain at least one special character.', 'danger')
-        else:
-            success = register_user(conn, username, email, password)
-            if success:
-                session['username'] = username
-                flash('Registration successful. Please connect a music platform.', 'success')
-                return redirect(url_for('connect'))
-            else:
-                flash('An error occurred during registration. Please try again.', 'danger')
-    return render_template('signup.html')
-
-#connecting platforms
 @app.route('/connect', methods=['GET', 'POST'])
 def connect():
     if request.method == 'POST':
         platform = request.form.get('platform')
         if platform == 'spotify':
-            return redirect(url_for('spotify_login'))
+            return redirect(url_for('loginSpotify'))
         elif platform == 'apple_music':
             ...
-            #flash('Apple Music connection is not implemented yet.', 'info')
+            # flash('Apple Music connection is not implemented yet.', 'info')
         elif platform == 'soundcloud':
             ...
             #flash('SoundCloud connection is not implemented yet.', 'info')
@@ -99,29 +157,6 @@ def connect():
             ...
             #flash('Other music platform connection is not implemented yet.', 'info')
     return render_template('connect.html')
-
-@app.route('/spotify_login')
-def spotify_login():
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session['spotify_token'] = token_info
-    flash('Spotify account connected successfully.', 'success')
-    return redirect(url_for('index'))
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash('Logged out successfully.', 'success')
-    return redirect(url_for('start_page'))
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
 @app.route('/profile')
 def profile():
@@ -140,9 +175,6 @@ def game():
     return render_template('game.html')
 
 if __name__ == '__main__':
-    conn = get_db_connection()
-    create_users_table(conn)
-    conn.close()
     app.run(debug=True, port=8080)
 
 
